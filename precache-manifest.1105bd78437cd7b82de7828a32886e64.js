0dc0499653c127d145c6dddae217b281de842978
Moong0122 <2015004802@hanyang.ac.kr>
2020-01-10 11:34:38 +0900

    Updates

---
+++ precache-manifest.1105bd78437cd7b82de7828a32886e64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dfde12911068e58eb0914efa2b4e9de",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d988abd42b61dff2e12e",
+    "url": "/movie_app_2020/static/css/main.53c1a3ae.chunk.css"
+  },
+  {
+    "revision": "2e3ab7344245c66bbfa2",
+    "url": "/movie_app_2020/static/js/2.5ee37bb2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.5ee37bb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d988abd42b61dff2e12e",
+    "url": "/movie_app_2020/static/js/main.4ad35046.chunk.js"
+  },
+  {
+    "revision": "c1e22dc80131c8cefb55",
+    "url": "/movie_app_2020/static/js/runtime-main.3d665dea.js"
+  }
+]);
