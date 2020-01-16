86c55484a7b71110f32ec742ff97fbee43d59118
Moong0122 <2015004802@hanyang.ac.kr>
2020-01-16 16:59:01 +0900

    building navigator & studying props

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./About.css";
 
 function About() {
   return <span>About this page: I built it because I love movies.</span>;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -49,4 +49,4 @@ class Home extends React.Component {
   }
 }
 
-export default App;
+export default Home;
